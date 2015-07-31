d9a68030bd70549eb9e4cb35021df782c11ba271
Paolo <boosta91@gmail.com>
2015-07-31 17:40:40 +0200

    rest routes, upvote function missing

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
 
 /* GET home page. */
@@ -7,3 +8,44 @@ router.get('/', function(req, res) {
 });
 
 module.exports = router;
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+// GET route to get posts
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
+
+    res.json(posts);
+  });
+});
+
+// POST route to store posts and populate the db
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
+});
+
+
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
+});
+
