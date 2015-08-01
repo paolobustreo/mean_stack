f60c31391174b88f1bb4bc8fd898a8a71d8739de
Paolo <boosta91@gmail.com>
2015-08-01 15:49:32 +0200

    upvotes working both for posts and comments

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -38,14 +38,60 @@ router.param('post', function(req, res, next, id) {
   query.exec(function (err, post){
     if (err) { return next(err); }
     if (!post) { return next(new Error('can\'t find post')); }
-
     req.post = post;
     return next();
   });
 });
 
-
 router.get('/posts/:post', function(req, res) {
   res.json(req.post);
+});
+
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post){
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error('can\'t find comment')); }
+    req.comment = comment;
+    return next();
+  });
+});
+
+router.get('/posts/:post/comments/:comment', function(req, res) {
+  res.json(req.comment);
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment){
+    if (err) { return next(err); }
+
+    res.json(comment);
+  });
 });
 
