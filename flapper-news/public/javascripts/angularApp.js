f57406d3ab5bbd7ff18c237460608e38a8a62822
Paolo <boosta91@gmail.com>
2015-08-04 16:30:26 +0200

    Express and AngularJs wired up, authentication for users is still missing

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -1,11 +1,44 @@
 var app = angular.module('flapperNews', ['ui.router'])
 
-app.factory('posts', [function(){
-  var o = {
-    posts: []
+// Posts factory 
+
+app.factory('posts', ['$http', function($http){
+  var o = {posts: []};
+  o.getAll = function() {
+    return $http.get('/posts').success(function(data){
+      angular.copy(data, o.posts);
+      });
+  };
+  o.create = function(post) {
+    return $http.post('/posts', post).success(function(data){
+      o.posts.push(data);
+    });
+  };
+  o.upvote = function(post) {
+    return $http.put('/posts/' + post._id + '/upvote')
+    .success(function(data){
+      post.upvotes += 1;
+    });
+  };
+  o.get = function(id) {
+  return $http.get('/posts/' + id).then(function(res){
+    return res.data;
+    });
+  };
+  o.addComment = function(id, comment) {
+    return $http.post('/posts/' + id + '/comments', comment);
+  };
+  o.upvoteComment = function(post, comment) {
+  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
+    .success(function(data){
+      comment.upvotes += 1;
+    });
   };
   return o;
+
 }]);
+
+// App configuration
 
 app.config([
 '$stateProvider',
@@ -15,58 +48,70 @@ function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {
     	url: '/home',
-      	templateUrl: '/home.html',
-      	controller: 'MainCtrl'
-    });
+      templateUrl: '/home.html',
+      controller: 'MainCtrl',
+      resolve: {
+          postPromise: ['posts', function(posts){
+            return posts.getAll();
+          }]
+      }
+  });
   $stateProvider
    	.state('posts', {
   		url: '/posts/{id}',
   		templateUrl: '/posts.html',
-  		controller: 'PostsCtrl'
-	});
+  		controller: 'PostsCtrl',
+      resolve: {
+        post: ['$stateParams', 'posts', function($stateParams, posts) {
+          return posts.get($stateParams.id);
+        }]
+      }
+  });
 
   $urlRouterProvider.otherwise('home');
 }]);
+
+// MainCtrl and PostCtrl
 
 app.controller('MainCtrl', [
 '$scope',
 'posts',
 function($scope, posts){
 	$scope.posts = posts.posts;
-  	
-  	$scope.addPost = function(){
-  		$scope.posts.push({
-    		title: $scope.title,
-    		link: $scope.link,
-    		upvotes: 0,
-    		comments: [
-    			{author: 'Joe', body: 'Cool post!', upvotes: 0},
-    			{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-  				]
-  		});
-  		$scope.title = '';
-  		$scope.link = '';
-		};
+  
+  $scope.addPost = function(){
+    if(!$scope.title || $scope.title === '') { return; }
+    posts.create({
+      title: $scope.title,
+      link: $scope.link,
+    });
+    $scope.title = '';
+    $scope.link = '';
+  };
 	
 	$scope.incrementUpvotes = function(post) {
-  		post.upvotes += 1;
-		};
+    posts.upvote(post);
+  };
 }]);
 
 app.controller('PostsCtrl', [
 '$scope',
 '$stateParams',
 'posts',
-function($scope, $stateParams, posts){
-	$scope.post = posts.posts[$stateParams.id];
+'post',
+function($scope, $stateParams, posts, post){
+	$scope.post = post;
 	$scope.addComment = function(){
-  		if($scope.body === '') { return; }
-  		$scope.post.comments.push({
-   			body: $scope.body,
-    		author: 'user',
-    		upvotes: 0
-  		});
-  $scope.body = '';
-};
+    if($scope.body === '') { return; }
+      posts.addComment(post._id, {
+      body: $scope.body,
+      author: 'user',
+    }).success(function(comment) {
+      $scope.post.comments.push(comment);
+    });
+    $scope.body = '';
+  };
+  $scope.incrementUpvotes = function(comment){
+    posts.upvoteComment(post, comment);
+  };
 }]);
-
